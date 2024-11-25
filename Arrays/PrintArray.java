8af32246bd202290ba4bc241b485b07c928231da
lingadurai03 <lingadurai033@gmail.com>
2024-11-25 16:03:10 +0530

    Add: rotate an array

---
+++ Arrays/PrintArray.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class PrintArray {
+
+    public static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
