871b16f8f2cb566bc6e179d7e81aefcb0cdc1e54
lingadurai03 <lingadurai033@gmail.com>
2024-12-03 19:27:12 +0530

    Add: max circular sub array

--- Arrays/MaximumProduct.java
+++ Arrays/MaximumProduct.java
@@ -1,5 +1,23 @@
 package Arrays;
 
+// Given an integer array, the task is to find the maximum product of any subarray.
+
+// Examples:
+
+// Input: arr[] = {-2, 6, -3, -10, 0, 2}
+// Output: 180
+// Explanation: The subarray with maximum product is {6, -3, -10} with product = 6 * (-3) * (-10) = 180
+
+// Input: arr[] = {-1, -3, -10, 0, 60}
+// Output: 60
+// Explanation: The subarray with maximum product is {60}.
+
+// Table of Content
+
+// [Naive Approach] By using two nested loops – O(n^2) Time and O(1) Space
+// [Expected Approach 1] Using minimum and maximum product ending at any index - O(n) Time and O(1) Space
+// [Expected Approach 2] By traversing in both directions - O(n) Time and O(1) Space
+
 public class MaximumProduct {
     public static void main(String[] args) {
         int[] arr = { -2, 6, -3, -10, 0, 2 };
--- Arrays/MaximumSubarraySum.java
+++ Arrays/MaximumSubarraySum.java
@@ -1,5 +1,24 @@
 package Arrays;
 
+// Examples:
+
+// Input: arr[] = {2, 3, -8, 7, -1, 2, 3}
+// Output: 11
+// Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+
+// Input: arr[] = {-2, -4}
+// Output: -2
+// Explanation: The subarray {-2} has the largest sum -2.
+
+// Input: arr[] = {5, 4, 1, 7, 8}
+// Output: 25
+// Explanation: The subarray {5, 4, 1, 7, 8} has the largest sum 25.
+
+// Table of Content
+
+// [Naive Approach] By iterating over all subarrays - O(n^2) Time and O(1) Space
+// [Expected Approach] Using Kadane's Algorithm - O(n) Time and O(1) Space
+
 public class MaximumSubarraySum {
     public static void main(String[] args) {
         int[] arr = { 2, 3, -8, 7, -1, 2, 3 };
